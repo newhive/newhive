d101bcbed867568a47290dad9fa38058c6131f20
Duffy Tilleman <duffy.tilleman@gmail.com>
2011-09-13 23:01:51 +0000

    fixed bug where pageviews change updated timestamp

--- state.py
+++ state.py
@@ -182,9 +182,9 @@ class Expr(Entity):
     def increment_counter(self, counter):
         assert counter in self.counters, "Invalid counter variable.  Allowed counters are " + str(self.counters)
         if self.has_key(counter):
-          self.update(**{counter: self[counter] + 1})
+          self.update(**{'updated': False, counter: self[counter] + 1})
         else:
-          self.update(**{counter: 1})
+          self.update(**{'updated': False, counter: 1})
         
 
 def tags_by_frequency(**query):
