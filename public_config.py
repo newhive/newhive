63c2b5d4b0d86d32815b3b0bdf45bc63cde75dee
Duffy Tilleman <duffy.tilleman@gmail.com>
2011-09-15 21:23:05 +0000

    removed port from intro_url in config

--- public_config.py
+++ public_config.py
@@ -1,4 +1,4 @@
 admins         = ['abram', 'andrew', 'cara', 'duffy', 'pconerly', 'zach']
-intro_url      = 'http://thenewhive.thenewhive.com:1515/welcome'
+intro_url      = 'http://thenewhive.thenewhive.com/welcome'
 intro_expr     = '4e708232ba28395e4f000011'
 signup_group   = 1
