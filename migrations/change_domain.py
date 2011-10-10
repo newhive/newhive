564456477a179c0aeadd67bafa312868252b8174
abram <abram@bf.(none)>
2011-10-10 15:48:17 -0700

    moved change_domain to different branch

--- migrations/change_domain.py
+++
@@ -1,9 +0,0 @@
-from state import *
-import config
-
-def update_domain(old_name='thenewhive.com'):
-    for e in Expr.search():
-        e.update(updated=False, domain=re.sub(old_name, config.server_name, e['domain']))
-
-    for u in User.search():
-        u.update(updated=False, sites=[u['name'] + '.' + config.server_name])
