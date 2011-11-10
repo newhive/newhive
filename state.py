3ff69a3a7137397171028ef55149f18aff6c177e
Duffy Tilleman <duffy.tilleman@gmail.com>
2011-11-10 01:57:01 +0000

    don't notify about new or updated private expressions unlesss
    the expression itself, and not just its owner, is starred.

--- state.py
+++ state.py
@@ -405,6 +405,7 @@ class Expr(Entity):
         return count
     share_count = property(get_share_count)
 
+    public = property(lambda self: self.get('auth') == "public")
 
 class File(Entity):
     cname = 'file'
@@ -563,14 +564,16 @@ class Star(Feed):
 class NewExpr(Feed):
     def create_me(self):
         super(NewExpr, self).create_me()
-        self.entity.owner.notify('starrers', self.id)
+        if self.entity.public:
+            self.entity.owner.notify('starrers', self.id)
         return self
 
 class UpdatedExpr(Feed):
     def create_me(self):
         super(UpdatedExpr, self).create_me()
         self.entity.notify('starrers', self.id)
-        self.entity.owner.notify('starrers', self.id)
+        if self.entity.public:
+            self.entity.owner.notify('starrers', self.id)
         return self
 
 class Referral(Entity):
