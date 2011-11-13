93aed0fa7ba5503ac5e19aa33908cb446480a0b7
Abram Clark <abram@thenewhive.com>
2011-11-13 19:35:47 +0000

    spelling

--- state.py
+++ state.py
@@ -628,7 +628,7 @@ def tags_by_frequency(**query):
             for t in d.get('tags_index'): tags[t] = tags.get(t, 0) + 1
     counts = [[tags[t], t] for t in tags]
     counts.sort(reverse=True)
-    return count
+    return counts
 
 def count(L):
     c = {}
