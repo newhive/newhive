669a93f2a0f5c6ef9b69294d2ba719ff789ff82c
Duffy Tilleman <duffy.tilleman@gmail.com>
2011-12-10 23:46:51 +0000

    new intro paragraph for homepage

--- ui_strings/en.py
+++ ui_strings/en.py
@@ -1,2 +1,8 @@
+intro_paragraph = """The New Hive makes it easy for you to express yourself
+online. Whether it's a journal, collection of recipes, portfolio or flyer, it's
+fun and easy to create and update all of your media and ideas in one ad-free
+space. Decide what you want to share to the community, the world and all your
+social media sites. What's your vision to change the world?"""
+
 embed_code_description = """Have media you'd like to share from another site? Paste in the URL's from vimeo or youtube here to include them in your expression. Or enter "embed" code found on the "share button" of many sites including- Grooveshark, googlemaps, soundcloud, bandcamp, tokbox, Flickr, paypal buttons, Ted Talks, and More! We also support iframes!"""
 embed_code_textarea = """Paste your url or embed code here."""
