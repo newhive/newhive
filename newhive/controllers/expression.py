8fa233f97c793145641273dc90f7d851e9d1c9a1
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-02-02 00:58:13 +0000

    featured tags now come from site_user['config']['featured_tags']

--- newhive/controllers/expression.py
+++ newhive/controllers/expression.py
@@ -142,8 +142,7 @@ class ExpressionController(ApplicationController):
     def _site_index(self, request, response, args={}):
         tag = args.get('tag')
         p1 = args.get('p1')
-        featured_tags = ["art", "seattle", "music", "poem", "occupy", "love", "drawing", "life", "story",
-            '2012', 'community', 'photography', 'poetry', 'words', 'food', 'travel', 'inspiration']
+        featured_tags = self.db.User.site_user['config']['featured_tags']
         tags = self.db.User.get_root().get('tags', [])
         response.context['system_tags'] = map(lambda t: {'url': "/home/" + t, 'name': t}, tags)
         people_tag = {'url': '/people', 'name': 'People'}
