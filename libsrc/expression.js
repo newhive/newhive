9e09ac03803df8fc7f50ddd8dc5f933deb55378f
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-04-20 06:24:19 +0000

    fixed flash jplayer in ff bug: protocols must match between page, media and swf file

--- libsrc/expression.js
+++ libsrc/expression.js
@@ -17,14 +17,14 @@ $(function() {
             cssSelectorAncestor: "#jp_container_" + $(this).data("index"),
             ready: function (event) {
               $(this).jPlayer("setMedia", {
-                mp3: $(this).data("url")
+                  mp3: $(this).data("url").replace(/^https?:/, window.location.protocol)
               });//.bind($.jPlayer.event.timeupdate, update_timer(this));
               //update_timer(this)(event);
             },
             loadeddata: update_timer(this),
             timeupdate: update_timer(this),
             ended: update_timer(this),
-            swfPath: server_url + "lib/",
+            swfPath: (window.location.protocol == "https:" ? server_url : insecure_server_url) + "lib/",
             supplied: "mp3",
             verticalVolume: true
         });
