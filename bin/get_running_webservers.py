57923b8eb7cffe996bbd0d2485ad10ea009afd96
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-05-04 20:58:00 +0000

    improved get_running_webservers script

--- bin/get_running_webservers.py
+++ bin/get_running_webservers.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
 
-import boto
+import boto, optparse, socket, subprocess
 
-GIT_FORMAT="""[remote "%(name)s"]
-  url = %(url)s:/var/www/newhive/
-	fetch = +refs/heads/*:refs/remotes/%(name)s/*"""
+FORMAT = {
+  'git': """[remote "{name}"]\n  url = {git_host}/var/www/newhive/\n	fetch = +refs/heads/*:refs/remotes/{name}/*"""
+  , 'human': "{name:<20} {url}"
+  }
+
 
 def get_running_webservers():
     con = boto.connect_ec2() # Relies on environment variable for AWS_KEY
@@ -15,6 +17,20 @@ def get_running_webservers():
 
 if __name__ == '__main__':
 
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--format", action="store", type="str", dest="format", default="human")
+    parser.add_option("--secure-port", action="store", type="int", dest="secure_port")
+    (options, args) = parser.parse_args()
+
     instances = get_running_webservers()
     for inst in instances:
-        print GIT_FORMAT % {'name': inst.tags.get('Name'), 'url': inst.public_dns_name}
+        # doesnt work yet, need to use ec2metadata script
+        #local = inst.ip_address == socket.gethostbyname(socket.gethostname())
+        local = False
+        dns = inst.public_dns_name
+        replacements = {
+                'name': inst.tags.get('Name')
+                , 'url': 'localhost' if local else dns
+                , 'git_host': ('' if local else dns + ':')
+                }
+        print FORMAT[options.format].format(**replacements)
