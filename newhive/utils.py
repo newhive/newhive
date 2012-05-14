bd18ddd8d8d6efddc576af493bdd13575e27a2bc
Abester <abram@dev-1.thenewhive.com>
2012-05-14 19:17:09 +0000

    Fixed utils.junkstr to not produce '/' for url friendliness

--- newhive/utils.py
+++ newhive/utils.py
@@ -43,7 +43,7 @@ def junkstr(length):
     """Creates a random base 64 string"""
 
     def chrange(c1, c2): return [chr(i) for i in range(ord(c1), ord(c2)+1)]
-    chrs = chrange('0', '9') + chrange('A', 'Z') + chrange('a', 'z') + ['.', '/']
+    chrs = chrange('0', '9') + chrange('A', 'Z') + chrange('a', 'z') + ['.', '+']
     return ''.join([chrs[random.randrange(0, 64)] for _ in range(length)])
 
 def lget(L, i, *default):
