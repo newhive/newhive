added5f60b746c283011032917bd24fd1c478a1b
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-07-09 23:52:41 +0000

    complete listing of top 100 users by impressions

--- newhive/controllers/analytics.py
+++ newhive/controllers/analytics.py
@@ -259,7 +259,15 @@ class AnalyticsController(ApplicationController):
         if view == 'top':
             # collection is a mongodb collection
             collection = newhive.analytics.overall_impressions(self.db, histogram=False)
-            collection.find({}, sort=[('value.views', -1)], limit=100)
+            user_list = collection.find({}, sort=[('value.views', -1)], limit=100)
+            users = []
+            for item in user_list:
+                user = self.db.User.fetch(item['_id'])
+                user.impressions = item['value']['views']
+                users.append(user)
+            response.context['users'] = users
+            return self.serve_page(response, 'pages/analytics/impressions_top.html')
+
         elif view == 'chart':
             hist, bin_edges = newhive.analytics.overall_impressions(self.db)
             response.context['data'] = list(hist[1:15])
