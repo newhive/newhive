c7f44f4ab683ca8dfaa71d1899998c7bd46d1df4
Abram Clark <abram@thenewhive.com>
2012-08-21 01:22:55 +0000

    added test config

---
+++ newhive/config/test_config.py
@@ -0,0 +1,23 @@
+# database_host   = '10.190.202.249'  # database-1
+database_host   = '10.99.10.179'
+database        = 'test'
+#database        = 'hive'
+dev_prefix      = 'dev-1'
+
+# dev-1 credentials
+aws_id          = 'AKIAJSU3W7EYEO4ZZCNA'
+aws_secret      = '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'
+
+s3_buckets      = ['dev-1-s1-newhive', 'dev-1-s2-newhive']
+asset_bucket    = 'dev-1-s0-newhive'
+
+use_ga          = False
+debug_mode      = True
+
+facebook_app_id = '153421698080835'
+facebook_client_secret = '53168c0305074b8ff82cab217d5043f9'
+
+email_server    = 'smtp.sendgrid.net'
+email_user      = 'thenewhive'
+email_password  = '2wK5rXh8PKDAMG6'
+#email_port      = 2525
