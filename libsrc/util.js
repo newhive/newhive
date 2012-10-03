f453529f9c15b0f3f3390c8c634e64f2e7db561b
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-10-03 20:38:04 +0000

    fix targets

--- libsrc/util.js
+++ libsrc/util.js
@@ -339,9 +339,7 @@ function link_target(i, a) {
 
     if(href && href.indexOf('http') === 0 && !re.test(href)) {
         a.attr('target', '_blank');
-    } else if (href == '') {
-        // do nothing
-    } else {
+    } else if (href && href.indexOf('http') === 0 && re.test(href)) {
         a.attr('target', '_top');
     }
 }
