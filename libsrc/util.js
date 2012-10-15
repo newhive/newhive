65e643db06e2fa4363c962131ceb03886a40a83f
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-10-15 22:52:43 +0000

    fixed profile thumb upload on signup page with invisible file input hack

--- libsrc/util.js
+++ libsrc/util.js
@@ -403,8 +403,11 @@ function asyncSubmit(form, callback, opts) {
 }
 
 function asyncUpload(opts) {
-    var target, form, opts = $.extend({ json : true, file_name : 'file', multiple : false, action: '/',
-        start : noop, success : noop, error: noop, data : { action : opts.post_action || 'file_create' } }, opts);
+    var target, form, opts = $.extend({
+        json : true, file_name : 'file', multiple : false, action: '/'
+        , start : noop, success : noop, error: noop, input_click: true
+        , data : { action : opts.post_action || 'file_create' } 
+    }, opts);
 
     var onload = function() {
         var frame = target.get(0);
@@ -427,13 +430,19 @@ function asyncUpload(opts) {
     var tname = 'upload' + Math.random();
     form = $('<form>').css({ position: 'absolute', left: -1000 }).addClass('async_upload')
         .attr({ method: 'POST', target: tname, action: opts.action, enctype: 'multipart/form-data' });
-    target = $("<iframe style='position : absolute; left : -1000px'></iframe>").attr('name', tname).appendTo(form).load(onload);
-    var input = $("<input type='file'>").attr('name', opts.file_name).change(function() { opts.start(); form.submit() }).appendTo(form);
+    target = $("<iframe style='position : absolute; left : -1000px'></iframe>")
+        .attr('name', tname).appendTo(form).load(onload);
+    var input = $("<input type='file'>").attr('name', opts.file_name)
+        .change(function() { opts.start(); form.submit() }).appendTo(form);
+    Hive.profile_upload_input = input;
     if(opts.multiple) { input.attr('multiple', 'multiple'); }
-    for(p in opts.data) $("<input type='hidden'>").attr('name', p).attr('value', opts.data[p]).appendTo(form);
+    for (p in opts.data) {
+        $("<input type='hidden'>").attr('name', p).attr('value', opts.data[p]).appendTo(form);
+    }
     form.appendTo(document.body);
     // It's a mystery why this timout is needed to make the upload dialog appear on some machines
-    setTimeout(function() { input.click() }, 50);
+    if (opts.input_click) setTimeout(function() { input.click(); }, 50);
+    return form;
 }
 
 function hovers_active(state){
