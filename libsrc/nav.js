59809e8e5555330f2375e56b279eb11c96e00d33
Duffy Tilleman <duffy.tilleman@gmail.com>
2012-11-01 17:34:10 +0000

    make About button in user nav on homepage link to about page

--- libsrc/nav.js
+++ libsrc/nav.js
@@ -148,6 +148,7 @@ Hive.Menus = (function(){
         var about_btn = $('<div>')
             .attr('id', 'about_btn')
             .attr('class', 'hoverable center text_btn black_active')
+            .click(function(){ window.location = server_url + 'home/page' })
             .append('<div>')
           .children()
             .attr('class', 'text')
