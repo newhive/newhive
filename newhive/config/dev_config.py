28eb1e5f69c9570aea9f79b834f2d741cad744f7
Abram <abram@dev-1.newhive.com>
2013-01-23 21:39:15 +0000

    fiddled with config

--- newhive/config/dev_config.py
+++ newhive/config/dev_config.py
@@ -1,25 +1,24 @@
-live_server     = False
-
+# database_host   = '10.190.202.249'  # database-1
 database_host   = '10.99.10.179'
-plain_port      = 1212
-ssl_port        = 1213
+database        = 'test'
+#database        = 'hive'
+#plain_port      = 1212
+#ssl_port        = 1213
 
-# 'live' credentials
-aws_id         = 'AKIAIWCIMFL2FCYQR25A'
-aws_secret     = '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'
-s3_buckets     = ['s1-thenewhive', 's2-thenewhive', 's3-thenewhive', 's4-thenewhive', 's5-thenewhive']
-asset_bucket = 's0-thenewhive'
+aws_id          = 'AKIAJSU3W7EYEO4ZZCNA'
+aws_secret      = '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'
+s3_buckets      = ['dev-1-s1-newhive', 'dev-1-s2-newhive']
+asset_bucket    = 'dev-1-s0-newhive'
 
-use_ga         = True
-#debug_mode     = True
+debug_mode      = True
+debug_unsecure  = False
 
 facebook_app_id = '153421698080835'
 facebook_client_secret = '53168c0305074b8ff82cab217d5043f9'
 
-email_server   = 'smtp.sendgrid.net'
-email_user     = 'thenewhive'
-email_password = '2wK5rXh8PKDAMG6'
+email_server    = 'smtp.sendgrid.net'
+email_user      = 'thenewhive'
+email_password  = '2wK5rXh8PKDAMG6'
+#email_port      = 2525
 
-ssl_ca         = '/etc/ssl/private/PositiveSSLChain.txt'
-ssl_cert       = '/etc/ssl/private/STAR_thenewhive_com.crt'
-ssl_key        = '/etc/ssl/private/STAR_thenewhive_com.key'
+admins          = ['abram', 'andrew', 'cara', 'duffy', 'zach', 'jack', 'fred']
