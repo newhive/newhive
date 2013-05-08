1709b8bae8b65f984a1970cb77cb11678a5abce5
Paul Sawaya <me@paulsawaya.com>
2013-05-07 18:49:35 -0700

    now upload snapshots to s3

--- newhive/snapshots.py
+++ newhive/snapshots.py
@@ -1,4 +1,7 @@
 import envoy
+import os
+from sys import platform
+
 from newhive import config
 
 class Snapshots(object):
@@ -9,6 +12,14 @@ class Snapshots(object):
             envoy.run("Xvfb :99 -screen scrn 1024x768x24")
     def take_snapshot(self,expr_id,out_filename,dimensions=(1024,768)):
         url = config.content_domain + '/' + str(expr_id)
-        envoy.run('webkit2png --feature=javascript --display=:99 --geometry=%s %s --output=%s' % (dimensions[0],dimensions[1],url))
+        if platform == 'linux' or platform == 'linux2':
+            envoy.run('webkit2png --feature=javascript --display=:99 --geometry=%s %s --output=%s' % (dimensions[0],dimensions[1],url))
+        elif platform == 'darwin':
+            # Mac support is super hacky, and mostly for local debugging.
+            envoy.run('webkit2png -C --clipwidth=%s --clipheight=%s --filename=out %s' % (dimensions[0],dimensions[1],url))
+            # Because the Mac webkit2png insists on giving you foo-clipped.png when you asked for foo, let's just rename it here.
+            os.rename('out-clipped.png',out_filename)
+
+
         # envoy.run(' '.join(['webkit2png', '--feature=javascript', '--display=:99', '--geometry=390 235',
         #     '--output=temp_small.png', url]))
