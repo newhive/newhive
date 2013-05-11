4c5b64253e68f5b309959cfdce5458b34aecda7f
Yan Z <yan@mit.edu>
2013-05-11 01:15:36 +0000

    Add pagination parameters to network trending searches

--- newhive/state.py
+++ newhive/state.py
@@ -80,7 +80,7 @@ class Database:
         if search.get('network'):
             results = viewer.feed_network(spec=spec, **args)
         elif search.get('network_trending'):
-            results, grouped_feed = viewer.feed_page_esdb(trending=True)
+            results, grouped_feed = viewer.feed_page_esdb(trending=True, at=start, limit=limit)
         elif search.get('featured'):
             results = self.Expr.page(self.User.root_user['tagged']['Featured'], **args)
         elif any(k in search for k in ('tags', 'phrases', 'text', 'user')):
