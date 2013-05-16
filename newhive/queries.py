dc54f7fa844c6c115527e0a0b5409ea860193070
Yan Z <yan@mit.edu>
2013-05-16 21:41:05 +0000

    Add tests for adding/deleting exprs

--- newhive/queries.py
+++ newhive/queries.py
@@ -5,20 +5,82 @@ import unittest
 db = state.Database(config)
 yan = db.User.fetch('yan', keyname='name')
 
+def mongo_total(col):
+    """return total size of a mongo collection"""
+    return col.search({}).count()
 
-class DBSyncTest(unittest.TestCase):
-    """test cases for syncing mongo with elasticsearch"""
+
+class ExprTest(unittest.TestCase):
+    """test cases for searching / syncing mongo with elasticsearch"""
     def setUp(self):
-        docs = [{'name': 'a_tremendously_unlikely_title',
-                'text': 'i hate atlas shrugged',
-                'auth': 'public',
-                'views': 40,
-                'title': 'a long rant'}]
-        self.docs = docs
+        """create some docs to search for"""
+        docs = [{'text': 'i hate atlas shrugged',
+                 'title': 'a long rant',
+                 'views': 20,
+                 'tags': '#unittest, #books'},
+                {'text': 'i ate the bones',
+                 'title': 'my favorite KFC commercial',
+                 'views': 100,
+                 'tags': '#unittest #popculture #sarcasm'}]
+        self.docs = []
+        self.size = len(docs)
         for d in docs:
-            yan.expr_create(d)
+            self.docs.append(TestExpr(d))
 
-    def tearDown(self):
+    def test_add_to_mongo(self):
+        """add these docs to mongo without indexing in es"""
+        count_before = mongo_total(db.Expr)
         for d in self.docs:
-            e = db.Expr.fetch(d['name'], keyname='name')
-            e.delete()
+            d.add_to_mongo(yan)
+        count_after = mongo_total(db.Expr)
+        self.assertEqual(count_before + self.size, count_after)
+
+    def test_sync_add(self):
+        count_before = db.esdb.get_total('expr-type')
+        db.esdb.sync_with_mongo()
+        count_after = db.esdb.get_total('expr-type')
+        self.assertEqual(count_before + self.size, count_after)
+
+    def test_sync_delete(self):
+        pass
+
+    def remove_docs(self):
+        for d in self.docs:
+            did = d['_id']
+            d.delete_from_mongo()
+            self.assertIsNone(db.Expr.fetch(did))
+
+    def runTest(self):
+        self.test_add_to_mongo()
+        self.remove_docs()
+
+class TestExpr(dict):
+    """class for temporary test expressions"""
+    def __init__(self, name, auth='public', views=0,
+                 stars=0, broadcasts=0, **kwargs):
+        super(TestExpr, self).__init__()
+        self['name'] = name
+        self['auth'] = auth
+        self['views'] = views
+        self['analytics'] = {'Star': {'count': stars},
+                             'Broadcast': {'count': broadcasts}}
+        self.expr = None
+        self.update(**kwargs)
+    def add_tags(self, tags):
+        self.update({'tags': tags})
+    def add_text(self, text):
+        self.update({'text': text})
+    def add_title(self, title):
+        self.update({'title': title})
+    def add_views(self, views):
+        self['views'] += views
+    def add_stars(self, stars):
+        self['analytics']['Star']['count'] += stars
+    def add_broadcasts(self, broadcasts):
+        self['analytics']['Broadcast']['count'] += broadcasts
+    def add_to_mongo(self, user):
+        self.expr = user.expr_create(self)
+    def delete_from_mongo(self):
+        self.expr.delete()
+        self.expr = None
+
