04519806a8413e3e288cc83b3e00370a998cb12e
Abram <abram@wirbu.(none)>
2013-05-31 13:12:44 -0700

    working recent page

--- newhive/controllers/community.py
+++ newhive/controllers/community.py
@@ -6,11 +6,20 @@ class Community(Controller):
         return self.db.query(request.args.get('q'), **paging_args)
 
     def home(self, tdata, request, username=None, **paging_args):
-        1/0
         return {
             'page_data': {
                 "cards": self.db.query('#Featured', viewer=tdata.user),
                 'header': ("Featured Expressions",),
+                'card_type': 'expr',
+            },
+            'title': "NewHive - Featured",
+        }
+
+    def recent(self, tdata, request, username=None, **paging_args):
+        return {
+            'page_data': {
+                "cards": self.db.query('#Recent', viewer=tdata.user),
+                'header': ("Recent Expressions",),
                 'card_type': 'expr',
             },
             'title': "NewHive - Featured",
