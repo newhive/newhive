407063af504437293caeeb03d8ec9a0fb126614a
Abram <abram@wirbu.(none)>
2013-07-31 03:26:49 -0700

    Added new thumb migration

---
+++ newhive/migrations/pending_add_thumb_222.py
@@ -0,0 +1,13 @@
+from newhive import state
+db = state.Database()
+
+def update_all_users():
+    for u in db.User.search({}): new_thumb(u)
+
+def new_thumb(u):
+    thumb_id = u.get('thumb_file_id')
+    if thumb_id: u.update(thumb_id=thumb_id)
+    f = db.File.fetch(thumb_id)
+    if not f: return # bail. # TODO: fall back to 'profile_thumb' url key
+    thumb_file = f.set_thumb(222, 222)
+    f.set_thumb(70, 70, thumb_file)
