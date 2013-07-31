3337d38541fbf91d1ef6d31077d6b526ff12b476
newduke <newguke@gmail.com>
2013-07-30 19:46:34 -0700

    Remove nav bar (except home page)

--- libsrc/ui/page/pages.js
+++ libsrc/ui/page/pages.js
@@ -1,14 +1,17 @@
 define([
     './expr',
+    './home',
     './edit_expr',
     './profile'
 ], function(
     expr,
+    home,
     edit_expr,
     profile
 ){ 
 	return {
     	expr: expr,
+        home: home,
         edit_expr: edit_expr,
     	profile: profile
 	}
