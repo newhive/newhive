ced25b801175f71894a2461c74ff76a72c5ca65d
newduke <newguke@gmail.com>
2013-08-30 18:53:50 -0700

    POLISH: rebublish on profile view

--- newhive/controllers/community.py
+++ newhive/controllers/community.py
@@ -4,6 +4,12 @@ from newhive.utils import dfilter, now, abs_url
 from newhive.controllers.controller import Controller
 from collections import Counter
 
+def deduped(item, dict):
+    if dict.get(item):
+        return False
+    dict['item'] = True
+    return item
+
 class Community(Controller):
     def search(self, tdata, request, **paging_args):
         return self.db.query(request.args.get('q'), **paging_args)
@@ -53,12 +59,41 @@ class Community(Controller):
             'form': 'create_account', 'title': "NewHive - Sign Up",
         }
 
-    # TODO: fix/test these functions for looking at other (nonowner) pages
     def expressions_public(self, tdata, request, owner_name=None, **args):
         owner = self.db.User.named(owner_name)
         if not owner: return None
-        spec = {'owner_name': owner_name, 'auth': 'public'}
-        cards = self.db.Expr.page(spec, tdata.user, **args)
+        spec = {'initiator': owner.id,
+            'class_name': {'$in': ['Broadcast','UpdatedExpr','NewExpr']}
+        }
+        # cards = self.db.Expr.page(spec, tdata.user, **args)
+        initial_limit = args.get('limit', 40)
+        limit = initial_limit
+        at = initial_at = args.get('at', 0)
+        card_ids = []
+        cards = []
+        d = {}
+        feeds = self.db.Feed.page(spec)
+        for f in feeds:
+            id = deduped(f['entity'], d)
+            if id:
+                card_ids.append(id)
+                card = self.db.Expr.fetch(id)
+                if not card:
+                    continue
+                if card['auth'] == 'public':
+                    cards.append(card)
+                if len(cards) >= initial_limit:
+                    break
+            # Possible optimization
+            # if len(card_ids) >= limit:
+            #     cards = filter(lambda e: e['auth'] == 'public', 
+            #         cards.extend(self.db.Expr.fetch(card_ids)))
+            #     limit = initial_limit - len(cards)
+            #     if limit == 0:
+            #         break
+            #     limit = max(10, limit)
+            #     card_ids = []
+
         profile = owner.client_view(viewer=tdata.user)
         return {
             'cards': cards, 'owner': profile, 'card_type':'expr',
