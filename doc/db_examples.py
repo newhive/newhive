37f2da5c049b14d1892ed1d0b26daa6965968844
newduke <newguke@gmail.com>
2013-10-15 15:34:40 -0700

    #491. Polish large feed card

--- doc/db_examples.py
+++ doc/db_examples.py
@@ -10,6 +10,10 @@ def exprs_with_embeds():
     return db.Expr.search({'apps': {'$elemMatch': {
         'type': 'hive.html', 'content': re.compile(r'<object|<embed', re.I)}}})
 
+def exprs_with_jplayer():
+    return db.Expr.search({'apps': {'$elemMatch': {
+    	'content': re.compile(r'jplayer', re.I)}}})
+
 def create_user(name):
 	db.User.create({"name": name,
 		"email":"me@somewhere.com",
