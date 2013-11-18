b4b748f839934928958959d654eae9e28b6a562c
newduke <newguke@gmail.com>
2013-11-18 15:03:52 -0800

    removed console.log

--- libsrc/ui/page.js
+++ libsrc/ui/page.js
@@ -425,7 +425,8 @@ define([
             on_file_upload('#profile_bg', '#bg_id_input'));
         // Click-through help text to appropriate handler
         $(".help_bar").on("click", function(e) {
-            console.log("ok" + e);$(this).next().trigger(e); })
+            $(this).next().trigger(e); 
+        });
 
         $('#user_update_form button[name=cancel]').click(function(e) {
             o.controller.open('expressions_public',
