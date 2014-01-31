eb3f1d26a00cd56c14741423c741816c7477fee3
newduke <newguke@gmail.com>
2014-01-31 14:19:08 -0800

    Fix/cleanup of query string, deferred load

--- libsrc/server/context.js
+++ libsrc/server/context.js
@@ -125,6 +125,10 @@ define([
     //   object constructor, see TODO-cleanup-object in text/stringjay
     o.query_attrs = function(scope, route_name, query){
         var args = get_route_args(Array.prototype.slice.call(arguments, 1));
+        query = $.map(query.split("&"),function(e) {
+            return $.map(e.split("="),function(k) {
+                return encodeURIComponent(k)}).join("=")
+        }).join("&");
         return attrs(route_name, args, query, false);
     };
 
@@ -286,7 +290,7 @@ define([
     o.parse_query = function(route_args){
         var url = window.location.toString();
         o.query = js.parse_query(url);
-        o.query_raw = url.substring((url + '?').indexOf('?') + 1);
+        o.search_query = o.query.q || "";//url.substring((url + '?').indexOf('?') + 1);
         if (!route_args)
             if (o.page_data && o.page_data.cards_route)
                 route_args = o.page_data.cards_route.route_args;
@@ -298,10 +302,12 @@ define([
             if (query_from_route != "") {
                 // TODO: merge the raw query attrs (removing dupes) 
                 // with the generated ones.
-                o.query_raw += 
-                    ((o.query_raw != "") ? "&" : "") + "q=" + query_from_route;
+                o.search_query = o.search_query + query_from_route;
+                    // ((o.search_query != "") ? "&" : "") + "q=" + query_from_route;
             }
         }
+        if (o.search_query != "")
+            o.search_query = "q=" + o.search_query;
 
         // Save error message and remove it from hash args
         // Note, we can't put the error info into query args, because altering the URL
