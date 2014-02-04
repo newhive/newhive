14d87143fac39f0f69222be276ea00de47d6e01b
Abram <abram@wirbu.(none)>
2014-02-03 20:29:22 -0800

    added cloudfront for assets #814

--- newhive/config/__init__.py
+++ newhive/config/__init__.py
@@ -13,18 +13,23 @@ def url_host(on_main_domain=True, port=None, secure=False):
     return domain if port == '' else domain + port
 
 # TODO-cleanup: remove duplicate logic with url_host
-def abs_url(path='', secure=False, domain=None, subdomain=None):
-    """Returns absolute url for this server, like 'https://thenewhive.com:1313/' """
+# relative=True should be used everywhere possible
+def abs_url(path='', secure=False, domain=None, subdomain=None, relative=False):
+    """Returns absolute url for this server, like
+       'https://thenewhive.com:1313/'.
+       relative=True leaves out protocol """
     domain = domain or server_name
     if domain.find('.' + server_name) > -1:
         (subdomain, domain) = domain.split('.', 1)
     if dev_prefix: domain = dev_prefix + '.' + domain
-    ssl = secure # or always_secure ## no longer used
-    proto = 'https' if ssl else 'http'
-    port = ssl_port if ssl else plain_port
+    if relative:
+        proto = ''
+    else:
+        proto = 'https:' if secure else 'http:'
+    port = ssl_port if secure else plain_port
     port = '' if port == 80 or port == 443 else ':' + str(port)
     return (
-        proto + '://' +
+        proto + '//' +
         (subdomain + '.' if subdomain else '') +
         domain + port + 
         '/' + re.sub('^/', '', path)
