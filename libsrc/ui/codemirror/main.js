8980e773011b0fb81d6e344435594a77a446b55e
Abram <abram@wirbu.(none)>
2014-03-06 18:48:20 -0800

    started code editor app

---
+++ libsrc/ui/codemirror/main.js
@@ -0,0 +1,8 @@
+define([
+    './codemirror'
+    ,'js!./addon/edit/matchbrackets.js'
+    ,'js!./addon/comment/continuecomment.js'
+    ,'js!./addon/comment/comment.js'
+    ,'js!./mode/javascript/javascript.js'
+    ,'js!./mode/css/css.js'
+], function(cm){ return cm })
