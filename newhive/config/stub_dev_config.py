b0f59b9e236859bad1b1f1a152f6dfedc41279b0
newduke <newguke@gmail.com>
2014-04-14 13:33:57 -0700

    WIP: cp_expr, lazy db evaluation

---
+++ newhive/config/stub_dev_config.py
@@ -0,0 +1,9 @@
+from config_common import *
+from dev_config import *
+database_host   = 'dev.newhive.com'
+
+# if needed, copy these from a live server, then delete immediately afterwards
+# TODO-dev-feature: ask for username and password if missing DB credentials
+# database_host   = 
+# aws_id         = 
+# aws_secret     = 
