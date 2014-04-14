a1dbc8abd9e3c78b6f0612ba97d0720031a80d61
newduke <newguke@gmail.com>
2014-04-14 13:35:51 -0700

    changed db key names

--- bin/cp_expr.py
+++ bin/cp_expr.py
@@ -9,8 +9,8 @@ from newhive.utils import MemoDict
 import newhive.config.tmp_live_config as live_config
 import newhive.config.stub_dev_config as dev_config
 dbs = MemoDict({
-    "live_db": lambda: state.Database(live_config)
-    ,"dev_db": lambda: state.Database(dev_config) 
+    "live": lambda: state.Database(live_config)
+    ,"dev": lambda: state.Database(dev_config) 
 })
 def cp_expr(expr, dest_db):
     new_expr = dest_db.Expr.new(expr)
