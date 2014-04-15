35e25d48e4686d2a80923e0bce0aad58403d8d02
Abram <abram@wirbu.(none)>
2014-04-14 19:23:11 -0700

    fixed s3 fix in dd00ab111cdb3e995c9f4fe493d8acd688c594b8

--- newhive/config/stub_dev_config.py
+++
@@ -1,9 +0,0 @@
-from config_common import *
-from dev_config import *
-database_host   = 'dev.newhive.com'
-
-# if needed, copy these from a live server, then delete immediately afterwards
-# TODO-dev-feature: ask for username and password if missing DB credentials
-# database_host   = 
-# aws_id         = 
-# aws_secret     = 
