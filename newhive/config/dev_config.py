4c9bb85e8fc44fe0254b451850dad3fe23511a4a
Abram <abram@wirbu.(none)>
2014-04-14 19:24:33 -0700

    ACTION REQUIRED: changed dev_config to local_config, and dev_config is for the dev server, consistent with live_config

--- newhive/config/dev_config.py
+++ newhive/config/dev_config.py
@@ -1,22 +1,10 @@
 from config_common import *
 
-database_host   = '127.0.0.1'
-database        = 'test'
-
-plain_port      = 1212
-ssl_port        = 1213
-debug_mode      = True
-debug_unsecure  = False
-
-# Uncomment to run as live server
-# sudo service apache2 restart
-# plain_port      = 80
-# ssl_port        = 443
-# debug_mode      = False
-
-server_name     = 'office.newhive.com'
-content_domain  = 'office.tnh.me'
 dev_prefix      = None
+
+# TODO-dev-security: move credentials into non-version-controlled file
+database_host   = 'mongodb://dev:2GlebTyo@dev.newhive.com/test'
+database        = 'test'
 
 aws_id          = 'AKIAJSU3W7EYEO4ZZCNA'
 aws_secret      = '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'
@@ -30,14 +18,9 @@ cloudfront_domains = {
     'asset': None
 }
  
-debug_unsecure  = True
-
 facebook_app_id = '153421698080835'
 facebook_client_secret = '53168c0305074b8ff82cab217d5043f9'
 
 email_server    = 'smtp.sendgrid.net'
 email_user      = 'thenewhive'
 email_password  = 'fadty345shd90'
-#email_port      = 2525
-
-#beta_testers = admins          = ['abram', 'andrew', 'cara', 'duffy', 'zach', 'jack', 'fred', 'newduke']
