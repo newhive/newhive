719ef85e9165c51d0f4704f145d07881ec650d8b
Abram <abram@wirbu.(none)>
2014-04-14 19:24:33 -0700

    ACTION REQUIRED: changed dev_config to local_config, and dev_config is for the dev server, consistent with live_config

--- newhive/config/tmp_live_config.py
+++ newhive/config/tmp_live_config.py
@@ -3,6 +3,9 @@ from live_config import *
 
 # if needed, copy these from a live server, then delete immediately afterwards
 # TODO-dev-feature: ask for username and password if missing DB credentials
-# database_host   = 
-# aws_id         = 
-# aws_secret     = 
+#     database_host =  ...
+#     aws_id =  ...
+#     aws_secret = ... 
+database_host   = 'mongodb://live:denjiafijUt1@jack.mongohq.com:10027/live-newhive'
+aws_id         = 'AKIAJLZRMQPQXT732WSA'
+aws_secret     = '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'
