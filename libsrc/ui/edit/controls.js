58be5f1f1a987b3266a88d7570713a2aca381852
Abram <abram@wirbu.(none)>
2014-05-19 23:04:05 -0700

    fixed controls menu y_offset in editor

--- libsrc/ui/edit/controls.js
+++ libsrc/ui/edit/controls.js
@@ -142,7 +142,7 @@ o.Controls = function(app, multiselect, delegate) {
             o.appendButton(x) } )); };
     o.hover_menu = function(handle, drawer, opts) {
         return u.hover_menu(handle, drawer, $.extend({
-            auto_height: false }, opts))
+            auto_height: false, offset_y: 5 }, opts))
     };
     o.single = function() {
         return (env.Selection.count() == 1) ? o.app.sel_app() : false }
