cc3321bfb4be3397b3b0f2f04be4fb5dc4ccf7cf
newduke <newguke@gmail.com>
2014-05-23 14:54:32 -0700

    Adding scripts

---
+++ bin/git_grep.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+# Usage: add the following line to your ~/.bashrc
+# source ~/src/newhive/newduke/bin/git_grep.sh
+
+function open_file {
+    which e && e $* || $EDITOR $*
+}
+# TODO: comment me
+function git_grep {(
+    path_match=""
+    filter_broken="/history.js|/jquery-1|/old|/broken|/curl|google_closure.js|/d3/|codemirror.js|jquery-ui|/jquery/"
+    while true; do
+        case "$1" in
+            -c) case=true;shift;;
+            -o) open=true;shift;;
+            -n) search_name=true;shift;;
+            -f) force=true;shift;;
+            -i) filter_broken="";shift;;
+            # -a) all=true;shift;;
+            -p) shift; path_match=$1; shift;;
+            *) break 2;
+        esac
+    done
+
+    case_flag='-i'
+    if [ $case ]; then case_flag=''; fi
+
+    if [ $search_name ]; then
+        git ls-files "$path_match" | grep $case_flag $* > ~/.efffiles
+        if [ $filter_broken ]; then
+            grep -E -v $filter_broken ~/.efffiles > ~/.efffiles_temp
+            mv -f ~/.efffiles_temp ~/.efffiles
+        fi
+        # echo "git ls-files \"$path_match\" | grep $case_flag $* > ~/.efffiles"
+    else
+        git grep $case_flag -n --no-color $* -- "$path_match" > ~/.efffiles
+        if [ $filter_broken ]; then
+            grep -E -v $filter_broken ~/.efffiles > ~/.efffiles_temp
+            mv -f ~/.efffiles_temp ~/.efffiles
+        fi
+        # echo "git grep $case_flag -n --no-color $* -- \"$path_match\" > ~/.efffiles"
+    fi
+
+    # list (and number) the matches
+    effpat=$(echo $@|awk '{print $NF}')
+    echo $effpat >> ~/.eff.log
+    grep $case_flag --color $effpat -n ~/.efffiles
+
+    if [ $open ] && ( [ $force ] || [ 1 == $(wc -l < ~/.efffiles) ] ); then
+        open_nth 1
+    fi
+)}
+
+# nth filename:lineno: in ~/.efffiles
+function nnn {(
+    line=$1
+    if [ -z $line ]; then
+        line="1"
+    fi
+    echo $(sed ${line}'q;d' $HOME/.efffiles | awk '{print $1}'|sed 's/\(:[0-9]\+:\).*/\1/')
+)}
+# print the nth filename
+function nn {(
+    echo $(nnn $1 | sed 's/:[0-9]\+://')
+)}
+# open nth file in ~/.efffiles
+function open_nth {(
+    open_file `nnn $1`
+)}
+
---
+++ bin/git_grep_nd.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+# Usage: add the following line to your ~/.bashrc
+# source ~/src/newhive/newduke/bin/git_grep_nd.sh
+
+source git_grep.sh
+
+alias a="git_grep"
+alias n='a -o -n'
+alias nh='a -o -p "*.html" -n'
+alias ns='a -o -p "*.scss" -n'
+alias np='a -o -p "*.py" -n'
+alias nj='a -o -p "*.js" -n'
+alias ah='a -o -p "*.html"'
+alias as='a -o -p "*.scss"'
+alias ap='a -o -p "*.py"'
+alias aj='a -o -p "*.js"'
+# Search python definition
+function ad {(
+    grepvar=$1
+    shift
+    a -o -p "*.py" $* "def[^(]*$grepvar"
+)}
+
+alias o='open_nth'
+alias o1='o 1'
+alias o2='o 2'
+alias o3='o 3'
+alias o4='o 4'
+alias o4='o 5'
+# rerun last list 
+alias on='grep . ~/.efffiles | grep -n -i `tail -1 ~/.eff.log`'
+# rerun last list, but only accept matches in last file part
+alias oe='grep . ~/.efffiles | grep -n -i `tail -1 ~/.eff.log`[^/]*$'
+
+alias routes='n newhive/routes.json'
+alias config='e `ff /config.py$`'
