76e8a32ac5804eb8227d79fa1ee7500bae1ed733
Abram <a@newhive.com>
2014-06-04 17:37:26 -0700

    fixed server_kill.sh

--- bin/server_kill.sh
+++ bin/server_kill.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-process_group=$(ps h -o pgrp $(pgrep server.py))
-ps ah -o pid,pgrp | egrep ' '$process_group'$' | cut -d' ' -f1 | xargs kill
+process_group=$(ps h -o pgrp $(pgrep server.py) | head -n 1)
+ps ah -o pgrp,pid | egrep '^'$process_group' ' | awk '{print $2}' | xargs kill -9
