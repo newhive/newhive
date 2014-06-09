61c39bacd5e0cd32fa0c4cda6e6f4c18324adc89
Abram <a@newhive.com>
2014-06-09 15:19:36 -0700

    misc mobile doc and stufs

--- bin/kill_server.sh
+++ bin/kill_server.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-process_group=$(ps h -o pgrp $(pgrep server.py) | head -n 1)
-ps ah -o pgrp,pid | egrep '^'$process_group' ' | awk '{print $2}' | xargs kill -9
+pgrep -g $(ps h -o pgrp $(pgrep server.py)) | xargs kill -9
