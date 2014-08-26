95bba8fc41e162609db8546f42f65e0a5fca3fe8
Abram <abram@live-4.newhive.com>
2014-08-26 04:04:59 +0000

    super hacky catch around activity bug so logged in people can still use site

--- newhive/state.py
+++ newhive/state.py
@@ -789,6 +789,12 @@ class User(HasSocial):
         return pyes.filters.BoolFilter(should=f)
 
     def activity(self, **args):
+        try:
+            return self.activity_bug(**args) 
+        except Exception as e:
+            print e
+
+    def activity_bug(self, **args):
         if not self.id: return []
         # TODO-feature: create list of exprs user is following comments on in
         # user record, so you can leave a comment thread
