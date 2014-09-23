ef8e85c8b25fe14b91f18256393ed1c5eec3b904
Abram Clark <abram@spez.local>
2014-09-23 13:24:23 -0700

    WIP: jslump fixes

--- libsrc/jslump/lump.js
+++ libsrc/jslump/lump.js
@@ -3,24 +3,25 @@ function (when, sequence, getCtx) {
     'use strict'
 
     var pluginParts = function(id) {
-        var delPos = id.indexOf('!');
+        var delPos = id.indexOf('!')
         return {
             resourceId: id.substr(delPos + 1),
             // resourceId can be zero length
             pluginId: delPos >= 0 && id.substr(0, delPos)
-        };
+        }
     }
 
     function lump(ids, io, parentCtx){
         ids.map(function(id){
+            console.log('starting ', id)
             var ctx = getCtx(id, parentCtx)
             sequence([getPlugin, toAmdText, info], ctx)
         })
 
         function getModule(id){
-            var dfd = when.defer();
-            parentCtx.require([id], dfd.resolve, dfd.reject);
-            return dfd.promise;
+            var dfd = when.defer()
+            parentCtx.require([id], dfd.resolve, dfd.reject)
+            return dfd.promise
         }
 
         function getPlugin(ctx){
@@ -38,44 +39,50 @@ function (when, sequence, getCtx) {
         }
 
         function info(ctx){
-            console.log(ctx.absId, ctx)
+            console.log(ctx.absId)
         }
 
         function toAmdText(ctx){
-            var plugin, resCfg, pio;
-            var dfd = when.defer();
+            console.log('AOEU')
+            var plugin, resCfg, pio
+            var dfd = when.defer()
 
             if(ctx.plugin){
+                console.log('plugin')
                 // grab plugin-specific config, if specified
                 if (ctx.pluginId && parentCtx.config.plugins[ctx.pluginId]){
                     resCfg = parentCtx.config.plugins[ctx.pluginId]
                 }
                 else {
-                    resCfg = fileCtx.config;
+                    resCfg = ctx.config
                 }
+                console.log(resCfg)
                 pio = {
                     write: resolve,
                     read: function (absId, callback, errback) {
-                        var url = parentCtx.toUrl(absId);
-                        when(io.readFile(url), callback, errback);
+                        var url = parentCtx.toUrl(absId)
+                        when(io.readFile(url), callback, errback)
                     },
                     error: dfd.reject,
                     warn: (console.warn ? console.warn : console.log).bind(console),
                     info: console.log.bind(console)
                 }
+                console.log('compiling...')
                 plugin.compile(
                     ctx.pluginId,
                     ctx.resourceId,
                     parentCtx.require, pio,
                     resCfg
-                );
+                )
+                console.log('compiled')
             }
             else {
+                console.log('not with plugin')
                 // get the text of the module
-                 when(io.readModule(ctx), resolve, dfd.reject);
+                 when(io.readModule(ctx), resolve, dfd.reject)
             }
 
-            return dfd.promise;
+            return dfd.promise
 
             function resolve(val){
                 ctx.text = val
