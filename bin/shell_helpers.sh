3caafc141e8218db7f426787ff5e09e61cef6f96
Abram <a@newhive.com>
2014-10-24 23:17:08 -0700

    Re-organized and beautified shell_helpers.sh
    Conflicts:
    	bin/shell_helpers.sh

--- bin/shell_helpers.sh
+++ bin/shell_helpers.sh
@@ -9,6 +9,9 @@ elif [ -L ${BASH_SOURCE[0]} ]; then
 else 
     NEWHIVE_HOME=$( dirname "${BASH_SOURCE[0]}" )/..
 fi
+
+### BEGIN find_files in git repo helpers ###
+############################################
 source $NEWHIVE_HOME/bin/git_grep.sh
 
 filter_broken="${filter_broken}|/titanium/|history/history|/jquery-1|/old|/broken|/curl|google_closure.js|/d3/|codemirror.js|jquery-ui|/jquery/|/codemirror/|mobile/|zepto-"
@@ -40,12 +43,17 @@ alias on=rerun_results
 alias oe=filepart_results
 # rerun last list, filtered through grep
 alias og=grep_results
+### END   find_files in git repo helpers ###
+############################################
 
 # git root dir
-alias groot="git rev-parse --show-toplevel"
-alias cdg='cd $(groot)'
-alias routes='e `groot`/newhive/routes.json'
+alias groot='git rev-parse --show-toplevel'
+alias cdg='cd $(groot)' # top level of current repo
+alias cdnh='cd $NEWHIVE_HOME' # go to default repo from outside 
+
 alias ff='find|grep -i'
+
+alias routes='e `groot`/newhive/routes.json'
 alias config='e $(groot)/newhive/config/config.py'
 alias config_common='e $(groot)/newhive/config/config_common.py'
 
@@ -61,3 +69,63 @@ alias newhive='(cdg; ./server.py)'
 alias killserver='psk server.py'
 alias rrr='reset; cdg; killserver; newhive'
 
+# shell shortcuts
+alias his='history'
+alias wwhich='echo $PATH|tr : " "|xargs find|grep -i'
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+alias ers='e ~/.bashrc'
+alias rs='source ~/.bashrc'
+
+# basic linux utils
+alias m='less -R'
+alias xx='chmod 755'
+alias xr='chmod 644'
+alias open='xdg-open'
+alias gi='grep -i'
+
+### BEGIN git_stuff ###
+#######################
+alias g='git'
+# git root dir
+# pre-submit checks
+alias pre="st;git diff|grep -n 'print\|!!\|bugbug\|TODO'"
+alias bm='git checkout master'
+alias bnd='git checkout newduke'
+alias bstage='git checkout staging'
+alias gC='git commit'
+alias st='git status'
+alias gpp='git pull && (cd `groot`;git submodule update) && git push'
+# alias gbranch='git status|grep branch|awk '"'"'{print $4}'"'"
+# alias gmerge='~/bin/gitmerge.sh master `gbranch`'
+
+git_branch() {
+    b=$(git symbolic-ref HEAD 2> /dev/null);
+    if [ $b ]; then echo -n "${b##refs/heads/}"; fi
+}
+
+# git binary search.  check out the a commit $1 commits back from HEAD
+function ghis {(
+    branch=$2
+    if [ -z $branch ]; then
+        branch="master"
+    fi
+    git checkout $branch > /dev/null 2>&1
+    git log --pretty=oneline|head -$1|tail -1|awk '{print $1}'
+)}
+function gbin {
+    git checkout `ghis $*`
+}
+
+# git log, print the last $1 commits, 1 line per commit
+function glog {
+    lines=$1
+    if [ -z $lines ]; then
+        lines="20"
+    fi
+    git log --pretty=oneline|head -$lines
+}
+### END   git_stuff ###
+#######################
