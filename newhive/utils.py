a217905dc84841096a90c46777a8c7f1e26671a9
Abram <a@newhive.com>
2014-11-18 13:21:57 -0800

    WIP: identify user in content domain requests

--- newhive/utils.py
+++ newhive/utils.py
@@ -468,13 +468,13 @@ def modify_query(url, d):
 def set_cookie(response, name, data, secure = False, expires = True):
     expiration = None if expires else datetime(2100, 1, 1)
     max_age = 0 if expires else None
-    response.set_cookie(name, value = data, secure = secure,
+    response.set_cookie(name, value = data, secure = secure, httponly=True,
         # no longer using subdomains
-        #domain = None if secure else '.' + config.server_name, httponly = True,
-        expires = expiration)
+        #domain = None if secure else '.' + config.server_name
+        expires = expires)
 def get_cookie(request, name): return request.cookies.get(name, False)
-def rm_cookie(response, name, secure = False):
-    response.delete_cookie(name, domain=None)
+def rm_cookie(response, name):
+    response.delete_cookie(name)
 
 
 def local_date(offset=0):
