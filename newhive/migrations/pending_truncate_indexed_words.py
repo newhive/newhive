93e00ac6bca3be25f3d2915341d9250d0c317f8a
Abram <abram@dev.newhive.com>
2014-12-05 17:10:52 +0000

    Added example in comment for iteratively running migration if random fails occur

--- newhive/migrations/pending_truncate_indexed_words.py
+++ newhive/migrations/pending_truncate_indexed_words.py
@@ -2,6 +2,16 @@ from newhive import state
 db = state.Database()
 from newhive.utils import Apply
 
+# in case migration repeatedly fails mysteriously
+#while True:
+#    try:
+#        pending_truncate_indexed_words.migrate()
+#    except:
+#        print('failed again')
+#        time.sleep(10)
+#        continue
+#    break
+
 def migrate():
     return Apply.apply_continue(fixup, db.Expr, {})
 
