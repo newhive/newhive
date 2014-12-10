f01de847c0142efa193e9cc4f10726b9aa14f481
Abram <abram@dev.newhive.com>
2014-12-10 03:24:17 +0000

    Made expr name truncate to 80 chars, created a migration for this in pending_truncate_indexed_words and another one to truncate text_index in user records

--- newhive/migrations/pending_truncate_indexed_words.py
+++ newhive/migrations/pending_truncate_indexed_words.py
@@ -1,6 +1,7 @@
 from newhive import state
 db = state.Database()
 from newhive.utils import Apply
+from newhive.mongo_helpers import mq
 
 # in case migration repeatedly fails mysteriously
 #while True:
@@ -12,10 +13,24 @@ from newhive.utils import Apply
 #        continue
 #    break
 
-def migrate():
-    return Apply.apply_continue(fixup, db.Expr, {})
+def migrate_exprs():
+    return Apply.apply_continue(fixup_exprs, db.Expr, {})
+def migrate_users():
+    return Apply.apply_continue(fixup_users, db.User, {})
+def migrate_exprs_name():
+    return Apply.apply_continue(expr_name, db.Expr, mq().js('this.name.length > 80'))
 
-def fixup(expr, dryrun=False):
+def fixup_exprs(expr, dryrun=False):
     expr.build_search(expr)
     expr.update(text_index=expr.get('text_index', ''), updated=False)
+    return True
+
+def expr_name(expr, dryrun=False):
+    expr.update(name=db.Expr.unused_name(expr.owner, expr['name'][0:80]),
+        updated=False)
+    return True
+
+def fixup_users(r, dryrun=False):
+    r.build_search(r)
+    r.update(text_index=r.get('text_index', ''), updated=False)
     return True
