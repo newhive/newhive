349d8aab589832c03ed53046b0ef03a88eca7c3e
Abram <a@newhive.com>
2015-04-23 02:27:28 -0700

    Moved visibility tracking code in controller

--- libsrc/ui/controller.js
+++ libsrc/ui/controller.js
@@ -39,44 +39,6 @@ define([
         o.init({route_name: 'home', client_method: 'home'})
     };  
 
-    // Make the visibility of the tab known
-    (function() {
-        var hidden = "hidden";
-
-        // Standards:
-        if (hidden in document)
-            document.addEventListener("visibilitychange", onchange);
-        else if ((hidden = "mozHidden") in document)
-            document.addEventListener("mozvisibilitychange", onchange);
-        else if ((hidden = "webkitHidden") in document)
-            document.addEventListener("webkitvisibilitychange", onchange);
-        else if ((hidden = "msHidden") in document)
-            document.addEventListener("msvisibilitychange", onchange);
-        // IE 9 and lower:
-        else if ('onfocusin' in document)
-            document.onfocusin = document.onfocusout = onchange;
-        // All others:
-        else
-            window.onpageshow = window.onpagehide 
-                = window.onfocus = window.onblur = onchange;
-
-        function onchange (evt) {
-            var v = 'visible', h = 'hidden', visibility = ''
-                evtMap = { 
-                    focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h 
-                };
-
-            evt = evt || window.event;
-            if (evt.type in evtMap)
-                visibility = evtMap[evt.type];
-            else        
-                visibility = this[hidden] ? "hidden" : "visible";
-            $("body").removeClass("hidden visible").addClass(visibility)
-        }
-        // set the initial state
-        onchange({type:(document.visibilityState == "visible") ? "focus" : "blur"})
-    })();
-
     o.init = function(route_args){
         window.c = context; // useful for debugging
         analytics.setup();
@@ -346,5 +308,43 @@ define([
         })(window);
     };
 
+    // Make the visibility of the tab known
+    (function() {
+        var hidden = "hidden";
+
+        // Standards:
+        if (hidden in document)
+            document.addEventListener("visibilitychange", onchange);
+        else if ((hidden = "mozHidden") in document)
+            document.addEventListener("mozvisibilitychange", onchange);
+        else if ((hidden = "webkitHidden") in document)
+            document.addEventListener("webkitvisibilitychange", onchange);
+        else if ((hidden = "msHidden") in document)
+            document.addEventListener("msvisibilitychange", onchange);
+        // IE 9 and lower:
+        else if ('onfocusin' in document)
+            document.onfocusin = document.onfocusout = onchange;
+        // All others:
+        else
+            window.onpageshow = window.onpagehide 
+                = window.onfocus = window.onblur = onchange;
+
+        function onchange (evt) {
+            var v = 'visible', h = 'hidden', visibility = ''
+                evtMap = { 
+                    focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h 
+                };
+
+            evt = evt || window.event;
+            if (evt.type in evtMap)
+                visibility = evtMap[evt.type];
+            else        
+                visibility = this[hidden] ? "hidden" : "visible";
+            $("body").removeClass("hidden visible").addClass(visibility)
+        }
+        // set the initial state
+        onchange({type:(document.visibilityState == "visible") ? "focus" : "blur"})
+    })();
+    
     return o;
 });
