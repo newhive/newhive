1324b91f825d707946d475b4293af11d39e77e98
Abram Clark <abram@spez.local>
2016-10-24 15:16:57 -0700

    Migrations for cleaning bogus files added, and migration for merging old DB from compose.io renamed.

--- newhive/migrations/m20161000_fix_db_migration.py
+++ newhive/migrations/m20161000_fix_db_migration.py
@@ -3,8 +3,8 @@ from newhive.mongo_helpers import mq
 from newhive.db_tools import dbs
 from pymongo.errors import DuplicateKeyError
 
-count = 0
 counts = {}
+fails = []
 
 def merge_db(config_name, do_write=False):
     db_remote = dbs(config_name)
@@ -31,10 +31,42 @@ def merge_collection(db_remote, cn, do_write=False):
 
     count = 0
     for r in cur:
-        local.update({'_id': r['_id']}, r, True)
-        #except DuplicateKeyError:
-        #    print r['_id'], 'duplicate!! shits'
+        try:
+            local.update({'_id': r['_id']}, r, True)
+        except DuplicateKeyError:
+            print r['_id'], 'duplicate!! shits'
         if not count % 100: print r[sort_attr]
         count += 1
     print 'finished merging %d records from %s' % (count, cn)
     counts[cn] = count
+
+# final merge for inexplicably missing records
+def merge_thorough(
+    db_remote, cn, id_list=[], do_write=False, report_interval=1000
+):
+    remote = db_remote.mdb[cn]
+    local = db.mdb[cn]
+
+    counts[cn] = 0
+    counts['added'] = 0
+
+    def merge_record(r):
+        if not r: return
+        if not counts[cn] % report_interval:
+            print 'checking %d %s' % (counts[cn], r['_id'])
+        if local.find_one(r['_id']) == None:
+            print 'adding to %s %s' % (cn, r['_id'])
+            try:
+                if do_write: local.insert(r)
+            except Exception as e:
+                print 'insert failed', e
+                fails.append(r)
+            counts['added'] += 1
+        counts[cn] += 1
+
+    if id_list:
+        for id in id_list: merge_record(remote.find_one(id))
+    else:
+        for r in remote.find(): merge_record(r)
+    print( 'finished adding %d out of %d to %s' %
+        (counts['added'], counts[cn], cn) )
